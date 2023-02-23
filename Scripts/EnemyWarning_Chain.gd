1bcf321c4a9eea3bc29196ba8ed3cabeb6d2a17e
Hoeloe <hoeloe@hotmail.co.uk>
2023-02-23 14:37:18 +0000

    Fixed drillrose issue - game still hitches, but no longer crashes or slows down.

--- Scripts/EnemyWarning_Chain.gd
+++ Scripts/EnemyWarning_Chain.gd
@@ -30,6 +30,9 @@ func start():
 	var length = path.curve.get_baked_length()
 	var count = round(length/gap)
 	var actual_gap = length/count
+	
+	var stamp_length = stamp_node.get_node("Telegraph/Direction/Length")
+	var stamp_direction = stamp_node.get_node("Telegraph/Direction")
 
 	var time_gap = 0
 	if duration > 0:
@@ -40,9 +43,10 @@ func start():
 	follower.rotate = orient_to_path
 	var rotation_offset = follower.global_rotation
 
+	var parent = get_parent()
 	# spawn the points
 	for i in count:
-		follower.offset = actual_gap*i
+		follower.unit_offset = i/count
 
 
 		# instantiate point warning and copy over properties
@@ -53,7 +57,7 @@ func start():
 		else:
 			spawned = EnemyManager.spawner_vector.instance()
 			spawned.speed = stamp_node.speed
-			spawned.get_node("Telegraph/Direction/Length").scale.x = stamp_node.get_node("Telegraph/Direction/Length").scale.x
+			spawned.get_node("Telegraph/Direction/Length").scale.x = stamp_length.scale.x
 
 		spawned.warning_sound = stamp_node.warning_sound
 		spawned.radius = stamp_node.radius
@@ -66,13 +70,14 @@ func start():
 
 
 		# Place in the map event and apply the transformation
-		get_parent().add_child(spawned)
+		parent.add_child(spawned)
 		spawned.global_position = follower.global_position
 		if  orient_to_path:
-			spawned.get_node("Telegraph/Direction").global_rotation = (follower.global_rotation - rotation_offset) + stamp_node.get_node("Telegraph/Direction").global_rotation
+			spawned.get_node("Telegraph/Direction").global_rotation = (follower.global_rotation - rotation_offset) + stamp_direction.global_rotation
 		else:
-			if  spawned.get_node_or_null("Telegraph/Direction") != null:
-				spawned.get_node("Telegraph/Direction").global_rotation = stamp_node.get_node("Telegraph/Direction").global_rotation
+			var node_direction = spawned.get_node_or_null("Telegraph/Direction")
+			if  node_direction != null:
+				node_direction.global_rotation = stamp_direction.global_rotation
 
 		#var new_rot = spawned.get_node("Telegraph/Direction").global_rotation
 		#print("SPAWN CHAIN ROTATION: ", rad2deg(new_rot))
